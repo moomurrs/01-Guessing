fe95392051d13135cf215f82cdf36dfff7e18dac
moomurrs <59849596+moomurrs@users.noreply.github.com>
2020-01-24 01:02:29 -0500

    Add Input Validation
    Input validation accounts for non-integers and also no input answer.

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from os import system, name
 
 # the playbool flag keeps track if the user wants to end the game
 playbool = True
+count = 0
 
-# fancy function to clear the terminal for new game
+# fancy function to clear the terminal for a new game
 def clear(): 
   
     # clear terminal for windows 
@@ -17,6 +18,34 @@ def clear():
     else: 
         _ = system('clear') 
 
+# Return a validated user input
+def inputValidation():
+    global count
+    suspiciousInput = ""
+    valid = False
+    inRange = False
+    while(not valid and not inRange):
+        suspiciousInput = input("Give me a guess: ")
+        count = count + 1
+        valid = suspiciousInput.isnumeric()
+        if(not valid):
+            print("Integers 1 - 100 only!")
+        else:
+            suspiciousInput = int(suspiciousInput)
+            if(isInRange(suspiciousInput)):
+                inRange = True
+            else:
+                inRange = False
+        
+    return suspiciousInput
+    
+# Helper function for inputValidation() to check is the integer is in range of 1 - 100.
+def isInRange(suspiciousInput):
+    if(suspiciousInput > 0 and suspiciousInput < 101):
+        return True
+    else:
+        return False
+
 while(playbool):
     # At the start of a game, reset stats
     count = 0
@@ -24,8 +53,7 @@ while(playbool):
     # Choose a random number from 1 to 100, inclusive
     randomNumber = random.randint(1, 101)
     print("I will guess a number between 1 and 100.\nYou will have to guess my number.")
-    guess = int(input("Enter your guess: "))
-    count = count + 1
+    guess = inputValidation()
 
     # loop to keep getting new guesses and provided feedback each time until the guess is right
     while(randomNumber != guess):
@@ -34,14 +62,13 @@ while(playbool):
         else:
             print("Too low")
         # Ask for a new guess
-        guess = int(input("Enter your guess: "))
-        count = count + 1
+        guess = inputValidation()
     
-    # When the guess is right, ask the user if they want play again. If the user wants to play again, the outer loop restarts.
+    # When the guess is right, ask the user if they want to play again. If the user wants to play again, the outer loop restarts.
     # If not, the program reaches the end.
     print("You guessed my number! It was: " + str(randomNumber))
-    print("You guessed " + str(count) + " times!")
-    again = input("Do you want to play again? Y/N")
+    print("You tried " + str(count) + " times!")
+    again = input("Do you want to play again? Y/N ")
     if(again == "Y"):
         playbool = True
         print("\n\n")
